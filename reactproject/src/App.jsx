c4b7c03f363522b9eae8bbbc2d403c9e090860be
SADDAM WASSAN <100377630+Saddamwassan@users.noreply.github.com>
2024-04-18 16:47:01 +0500

    added more cards with api's

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import Card from './components/card'
+import React,{useEffect, useState} from 'react'
+import Card from './components/card';
 
 export default function App() {
+
+    const [product,setProduct] = useState([])
+  //   async function fetchApi(){
+  //   const response = await fetch('https://dummyjson.com/products');
+  //   const result = await response.json();
+  //   if(result.products){
+  //     setProduct(prevProd=> prevProd = result.products)
+  //   }
+  // fetchApi();
+
+  useEffect(()=>{
+    fetch('https://dummyjson.com/products')
+      .then(res=>res.json())
+      .then(data=>setProduct(data.products))
+  },[])
+
+
+  console.log(product);
   return (
-    <>
-      <Card />
-    </>
+    <div>
+      <h1 className="producttext">Products</h1>
+    <div className='productPage'>
+      {
+        product.map(item => <Card data={item} key={item.id}/>)
+      }
+      
+    </div>
+    </div>
   )
 }
-
-// export default App
--- reactproject/src/components/card.jsx
+++ reactproject/src/components/card.jsx
@@ -1,14 +1,20 @@
 import React from "react"
-export default function Card(){
+export default function Card({data}){
     return(
     <div className="card">
-        <img className="s-image" src="https://m.media-amazon.com/images/I/71JmuWGUR0L._AC_UY218_.jpg"  alt="Fullmosa Compatible Apple Watch Metal Bands 45mm 44mm 42mm, Stainless Steel Mesh Loop Magnetic Clasp iWatch Band with TPU ..." data-image-index="10" data-image-load="" data-image-latency="s-product-image" data-image-source-density="1" />
-        <h3>Apple</h3>
-        <h4>Apple Watch in sale</h4>
+        <img className="s-image" src={data.images[0]}  alt="Fullmosa Compatible Apple Watch Metal Bands 45mm 44mm 42mm, Stainless Steel Mesh Loop Magnetic Clasp iWatch Band with TPU ..." data-image-index="10" data-image-load="" data-image-latency="s-product-image" data-image-source-density="1" />
+        <div className="textDiv">
+        <h3>{data.brand}</h3>
+        <h4>{data.category}</h4>
+        <ul>
+            features
+            <li>{data.description}</li>
+        </ul>
+        </div>
         <h2>$120</h2>
         <div className="btnDiv">
         <button className="cartbtn">ADD TO CART<i className="fa-solid fa-cart-shopping cart"></i></button>
-        <button className="forwardbtn"><i class="fa-solid fa-arrow-right"></i></button>
+        <button className="forwardbtn"><i className="fa-solid fa-arrow-right"></i></button>
         </div>
      </div>
     )
